94e45059a3aecb60e54bc2c19d0b6b55d3c12d97
Johannes Lüke <Johannes.lueke@fom-net.de>
2024-03-01 14:57:20 +0100

    chore: get dev env running

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
       open: true
     },
     plugins: [analog({
-       //ssr: false,
+       ssr: false,
        //static: true,
        prerender: {
         postRenderingHooks: [
