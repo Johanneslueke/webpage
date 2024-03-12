b69c06eb54d1c30042d93903bcb6655da5288b37
Johannes Lüke <Johannes.lueke@fom-net.de>
2024-03-12 14:13:03 +0100

    refactor: reload page

--- main/src/app/app.component.ts
+++ main/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, DestroyRef, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { SwUpdate, VersionEvent,VersionDetectedEvent , VersionInstallationFailedEvent , VersionReadyEvent , NoNewVersionDetectedEvent } from "@angular/service-worker";
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { tap } from "rxjs";
 
 @Component({
   selector: "main-root",
@@ -7,4 +10,33 @@ import { RouterOutlet } from "@angular/router";
   imports: [RouterOutlet],
   template: ` <router-outlet></router-outlet> `,
 })
-export class AppComponent {}
+export class AppComponent {
+
+  swUpdate = inject(SwUpdate);
+  destroy$ = inject(DestroyRef);
+
+  updateNewVersion(){
+     if(confirm("New version is available. Load new Version?")){
+      window.location.reload();
+     }
+  }
+
+  ngOnInit(){
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.versionUpdates.pipe(
+        tap( (event : VersionEvent)  => {
+          //VersionDetectedEvent | VersionInstallationFailedEvent | VersionReadyEvent | NoNewVersionDetectedEvent;
+          switch(event.type){
+            case 'VERSION_DETECTED': this.updateNewVersion(); break;
+            case 'VERSION_READY' : break;
+            case 'VERSION_INSTALLATION_FAILED': break;
+            case "NO_NEW_VERSION_DETECTED": break;
+            default:
+              break;
+          }
+        }),
+        takeUntilDestroyed(this.destroy$)
+      )
+    }
+  }
+}
