b3b8033823e394246a5ae7657139248a02c30451
Johannes Lüke <Johannes.lueke@fom-net.de>
2024-03-12 10:26:27 +0100

    feat(blog): display list of all available posts

--- main/src/app/pages/(home).page.ts
+++ main/src/app/pages/(home).page.ts
@@ -1,11 +1,35 @@
 import { Component } from "@angular/core";
 
 import { AnalogWelcomeComponent } from "./analog-welcome.component";
+import { NgFor } from "@angular/common";
+import { RouterOutlet, RouterLink } from "@angular/router";
+import { injectContentFiles } from "@analogjs/content";
+
+export interface PostAttributes {
+  title: string;
+  slug: string;
+  description: string;
+  coverImage: string;
+}
+
 
 @Component({
   selector: "main-home",
   standalone: true,
-  imports: [AnalogWelcomeComponent],
-  template: ` <main-analog-welcome /> `,
+  imports: [RouterOutlet, RouterLink, NgFor], 
+  template: `
+    <ul>
+      <li *ngFor="let post of posts">
+        <a [routerLink]="['/blog', 'posts', post.slug]">{{
+          post.attributes.title
+        }}</a>
+      </li>
+    </ul>
+  
+  `,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  readonly posts = injectContentFiles<PostAttributes>((contentFile) =>
+  contentFile.filename.includes('/src/content/blog/')
+);
+}
