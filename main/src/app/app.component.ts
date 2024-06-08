e34d3b523466620bbc5b3b0e0c2b3084961255cd
Johannes Lüke <Johannes.lueke@fom-net.de>
2024-06-08 14:14:14 +0200

    feat: add footer

--- main/src/app/app.component.ts
+++ main/src/app/app.component.ts
@@ -24,6 +24,23 @@ import {MainLayoutComponent, MainLayoutService } from '@projects/main-layout'
 >
   <projects-main-layout></projects-main-layout>
   <router-outlet></router-outlet> 
+  <footer class="footer p-10 bg-neutral text-neutral-content">
+  <nav>
+    <h6 class="footer-title">Services</h6> 
+    <a class="link link-hover">Consulting</a> 
+  </nav> 
+  <nav>
+    <h6 class="footer-title">Company</h6> 
+    <a class="link link-hover">About me</a>
+    <a class="link link-hover">Contact</a> 
+  </nav> 
+  <nav>
+    <h6 class="footer-title">Legal</h6> 
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
 </div>
  
   
